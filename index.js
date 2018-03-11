7e2e44a00390d4c24a8660de51b32615e96fc31c
emre <test@example.com>
2018-03-11 18:36:13 -0500

    Set up express and google oauth api

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
+const app = express();
+
+passport.use(new GoogleStrategy());
+
+
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+    console.log(`Server started at port ${port}`);
+});
