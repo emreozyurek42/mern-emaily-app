dd6995b027a8b69c7b5815a245aad0cad08a592c
emre <test@example.com>
2018-03-13 23:27:32 -0500

    Finished auth flow

--- index.js
+++ index.js
@@ -1,15 +1,27 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
 const auth = require('./routes/auth');
 const keys = require('./config/keys');
+require('./models/User');
 require('./services/passport');
 
 
-mongoose.connect(keys.mongoURI)
+
+mongoose.connect(keys.mongoURI, {useMongoClient: true})
 .then(() => console.log('MongoDB connected'))
 .catch((err) => console.log(err));
 
 const app = express();
+
+app.use(cookieSession({
+   maxAge: 30*24*60*60*1000,
+   keys: [keys.cookieKey]
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use('/auth', auth);
 
