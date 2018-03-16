736af6739895f754b00fe934d06b1b6204a5313b
emre <test@example.com>
2018-03-15 22:54:07 -0500

    Reducers and routes set up

--- services/passport.js
+++ services/passport.js
@@ -6,14 +6,14 @@ const keys = require('../config/keys');
 const User = mongoose.model('users');
 
 passport.serializeUser((user, done) => {
-   done(null, user.id);
+    done(null, user.id);
 });
 
 passport.deserializeUser((id, done) => {
-   User.findById(id)
-   .then(user => {
-       done(null, user);
-   });
+    User.findById(id)
+        .then(user => {
+            done(null, user);
+        });
 });
 
 
@@ -21,16 +21,16 @@ passport.use(new GoogleStrategy({
     clientID: keys.googleClientID,
     clientSecret: keys.googleClientSecret,
     callbackURL: '/auth/google/callback'
-}, (accessToken, refreshToken, profile, done) => {
-    User.findOne({ googleId: profile.id })
-     .then((existingUser) => {
-       if(existingUser) {
-           done(null, existingUser);
-       } else {
-        new User({googleId: profile.id })
-         .save()
-         .then(user => done(null, user));
-       }
-     })
-  })
+},
+    async (accessToken, refreshToken, profile, done) => {
+        const existingUser = await User.findOne({ googleId: profile.id })
+
+        if (existingUser) {
+            done(null, existingUser);
+        } else {
+            const user = await new User({ googleId: profile.id }).save()
+            done(null, user);
+        }
+
+    })
 );
