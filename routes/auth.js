57c59e4315a7aa5200d7f2648fc0b1c9ef08f837
emre <test@example.com>
2018-03-18 23:13:52 -0500

    Redirecting a user on auth

--- routes/auth.js
+++ routes/auth.js
@@ -7,15 +7,22 @@ router.get('/google', passport.authenticate('google', {
     scope: ['profile', 'email']
 }));
 
-router.get('/google/callback', passport.authenticate('google'));
+router.get(
+    '/google/callback', 
+    passport.authenticate('google'),
+    (req, res) => {
+        res.redirect('/surveys');
+    }
+);
 
 router.get('/api/logout', (req, res) => {
     req.logout();
-    res.send(req.user);
+    req.session = null;
+    res.redirect('/');
 });
 
 router.get('/api/current_user', (req, res) => {
     res.send(req.user);
-})
+});
 
 module.exports = router;
